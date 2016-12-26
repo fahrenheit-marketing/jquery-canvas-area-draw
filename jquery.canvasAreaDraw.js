709e9d25ee2bb8de67e9d4d480ac68cac402bbd3
zlo2k <zlo2k@bk.ru>
2016-12-26 23:05:53 +0600

    Update jquery.canvasAreaDraw.js
    Add global drag point

--- jquery.canvasAreaDraw.js
+++ jquery.canvasAreaDraw.js
@@ -12,7 +12,9 @@
 
         var points, activePoint, settings;
         var $reset, $canvas, ctx, image;
-        var draw, mousedown, stopdrag, move, resize, reset, rightclick, record;
+        var draw, mousedown, stopdrag, move, moveall, resize, reset, rightclick, record;
+        var dragpoint;
+        var startpoint = false;
 
         settings = $.extend({
             imageUrl: $(this).attr('data-image-url')
@@ -44,7 +46,7 @@
         $canvas.css({background: 'url(' + image.src + ')'});
 
         $(input).after('<br>', $canvas, '<br>', $reset);
-        
+
         reset = function () {
             points = [];
             draw();
@@ -57,6 +59,23 @@
             }
             points[activePoint] = Math.round(e.offsetX);
             points[activePoint + 1] = Math.round(e.offsetY);
+            draw();
+        };
+
+        moveall = function (e) {
+            if (!e.offsetX) {
+                e.offsetX = (e.pageX - $(e.target).offset().left);
+                e.offsetY = (e.pageY - $(e.target).offset().top);
+            }
+            if (!startpoint) {
+                startpoint = {x: Math.round(e.offsetX), y: Math.round(e.offsetY)};
+            }
+            var sdvpoint = {x: Math.round(e.offsetX), y: Math.round(e.offsetY)};
+            for (var i = 0; i < points.length; i++) {
+                points[i] = (sdvpoint.x - startpoint.x) + points[i];
+                points[++i] = (sdvpoint.y - startpoint.y) + points[i];
+            }
+            startpoint = sdvpoint;
             draw();
         };
 
@@ -99,6 +118,17 @@
             }
             x = e.offsetX;
             y = e.offsetY;
+
+            if (points.length >= 6) {
+                var c = getCenter();
+                ctx.fillRect(c.x - 4, c.y - 4, 8, 8);
+                dis = Math.sqrt(Math.pow(x - c.x, 2) + Math.pow(y - c.y, 2));
+                if (dis < 6) {
+                    startpoint = false;
+                    $(this).on('mousemove', moveall);
+                    return false;
+                }
+            }
 
             for (var i = 0; i < points.length; i += 2) {
                 dis = Math.sqrt(Math.pow(x - points[i], 2) + Math.pow(y - points[i + 1], 2));
@@ -144,7 +174,10 @@
             ctx.fillStyle = 'rgb(255,255,255)';
             ctx.strokeStyle = 'rgb(255,20,20)';
             ctx.lineWidth = 1;
-
+            if (points.length >= 6) {
+                var c = getCenter();
+                ctx.fillRect(c.x - 4, c.y - 4, 8, 8);
+            }
             ctx.beginPath();
             ctx.moveTo(points[0], points[1]);
             for (var i = 0; i < points.length; i += 2) {
@@ -163,6 +196,29 @@
 
         record = function () {
             $(input).val(points.join(','));
+        };
+
+        getCenter = function () {
+            var ptc = [];
+            for (i = 0; i < points.length; i++) {
+                ptc.push({x: points[i], y: points[++i]});
+            }
+            var first = ptc[0], last = ptc[ptc.length - 1];
+            if (first.x != last.x || first.y != last.y) ptc.push(first);
+            var twicearea = 0,
+                x = 0, y = 0,
+                nptc = ptc.length,
+                p1, p2, f;
+            for (var i = 0, j = nptc - 1; i < nptc; j = i++) {
+                p1 = ptc[i];
+                p2 = ptc[j];
+                f = p1.x * p2.y - p2.x * p1.y;
+                twicearea += f;
+                x += ( p1.x + p2.x ) * f;
+                y += ( p1.y + p2.y ) * f;
+            }
+            f = twicearea * 3;
+            return {x: x / f, y: y / f};
         };
 
         $(input).on('change', function () {
@@ -197,7 +253,10 @@
                 if (!(x1 - x0)) return {x: x0, y: y};
                 else if (!(y1 - y0)) return {x: x, y: y0};
                 var left, tg = -1 / ((y1 - y0) / (x1 - x0));
-                return {x: left = (x1 * (x * tg - y + y0) + x0 * (x * -tg + y - y1)) / (tg * (x1 - x0) + y0 - y1), y: tg * left - tg * x + y};
+                return {
+                    x: left = (x1 * (x * tg - y + y0) + x0 * (x * -tg + y - y1)) / (tg * (x1 - x0) + y0 - y1),
+                    y: tg * left - tg * x + y
+                };
             }(x, y, x0, y0, x1, y1), o.x >= Math.min(x0, x1) && o.x <= Math.max(x0, x1) && o.y >= Math.min(y0, y1) && o.y <= Math.max(y0, y1))) {
             var l1 = lineLength(x, y, x0, y0), l2 = lineLength(x, y, x1, y1);
             return l1 > l2 ? l2 : l1;
